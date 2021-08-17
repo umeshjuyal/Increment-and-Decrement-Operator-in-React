7c05fd4b6beff80d49ed8cee309f080cd55ddcf4
Umesh <umeshxz0000@gmail.com>
2021-08-17 21:27:16 +0530

    index.css & App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+// import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+
+  const [num,setNum]=useState(0);
+
+  const incNum =()=>{
+    setNum(num + 1);
+  };
+
+  const decNum = () => {
+    if(num > 0) {
+    setNum(num - 1);
+  }
+  else{
+    alert('Sorry,Zero Limit Reached')
+    setNum(0);
+  }
+};
+
+return (
+  <>
+  <div className='main_div'>
+    <div className='center_div'>
+      <h1> {num}</h1>
+      <div className='btn_div'>
+        <button onClick={incNum}>Increm</button>
+        <button onClick={decNum}>Decrem</button>
+        
+      </div>
     </div>
-  );
-}
+  </div>
 
-export default App;
+  </>
+)
+}
+export default App ;
+
